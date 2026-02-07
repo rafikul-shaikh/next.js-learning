fccb784c926a6ce0810410d4eaf8ba952b99a93c
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-02-07 11:32:02 +0530

    added dynamic routes

--- level-3/02_nested-routing-in-next.js/src/app/page.js
+++ level-3/02_nested-routing-in-next.js/src/app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <>
       <h1>Technical Agency</h1>
-      <Link href="/about">About</Link> <Link href="/services">Services</Link>
+      <Link href="/about">About</Link> 
+      <Link href="/services">Services</Link>
     </>
   );
 }
